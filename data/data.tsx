509ed1076ec63417580bc4cc8096256141150260
Lilit <baregamyanlilit36@gmail.com>
2026-05-18 22:19:31 +0400

    .

--- data/data.tsx
+++ data/data.tsx
@@ -6,7 +6,7 @@ export const program: any = [
     address: "https://maps.apple.com/place?map=explore&address=Charents+Street%2C+Vardenis%2C+Armenia&coordinate=40.185817%2C45.728211&name=Charents+Street",
   },
   {
-    time: "17:00",
+    time: "17:30",
     title: "Հարսանյաց սրահ",
     subt:'Bayazet Hall',
     address: "https://maps.apple.com/place?coordinate=40.366667,45.133333&name=Sayadyan%20112/2%20Gavar&map=explore",
@@ -20,8 +20,6 @@ export const txt1_1 =
 
 export const txt2 ="Խնդրում ենք նախապես տեղեկացնել\n Ձեր մասնակցության մասին մինչև \n Մայիսի 27-ը \n Սիրո՛վ սպասում ենք";
 export const txt2_1 ="Մասնակցության մասին Խնդրում ենք \n Տեղեկացնել մինչև \n Մայիսի 27-ը";
-export const txt3 =
-  "Նշանադրությունը տեղի կունենա \nԱրև ռեստորաննում \nժամը 17:30-ին";
 
 export const anim: any = {
   initial: { opacity: 0, y: 40 },
